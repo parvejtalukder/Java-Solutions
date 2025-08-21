388f8661b55babbf6e39d2cb7a431346c9f5573c
parvejtalukder <mail@parvejhusentalukder.com>
2025-08-21 20:49:13 +0600

    Update: Two Program Done for HW

--- saleryCalc/src/saleryCalc/Main.java
+++ saleryCalc/src/saleryCalc/Main.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // System.out.print("Enter the number of employees: ");
+        System.out.print("Enter the number of employees: ");
         int numberOfEmployees = scanner.nextInt();
         float[][] salaryIncrement = new float[numberOfEmployees][2];
 		float[] salaryIncremented = new float[numberOfEmployees];
         float totalSalary = 0, totalIncremented = 0, netDifference;
         for (int i = 0; i < numberOfEmployees; i++) {
-            // System.out.println("\nGive Me Info. for Employee No. " + (i + 1) + ":");
-            // System.out.print("Enter salary in BDT: ");
+            System.out.println("\nGive Me Info. for Employee No. " + (i + 1) + ":");
+            System.out.print("Enter salary in BDT: ");
             salaryIncrement[i][0] = scanner.nextFloat();
-            // System.out.print("Enter increment percentage: ");
+            System.out.print("Enter increment percentage: ");
             salaryIncrement[i][1] = scanner.nextFloat();
             totalSalary += salaryIncrement[i][0];
 			salaryIncremented[i] = salaryIncrement[i][0] + (salaryIncrement[i][0] * (salaryIncrement[i][1]/(float)100));
@@ -20,8 +20,7 @@ public class Main {
         }
 		netDifference = totalIncremented - totalSalary;
 		System.out.println();
-		System.out.printf("%.2f\n", netDifference);
-	    // System.out.printf("Company Have To Pay Extra %.2f BDT\n", netDifference);
+	    System.out.printf("Company Have To Pay Extra %.2f BDT\n", netDifference);
 		// System.out.println("www.parvejhusentalukder.com");
         scanner.close();
     }
