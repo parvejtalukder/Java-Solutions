2b77f4f5884265a32424b946394f31c316bd3686
parvejtalukder <mail@parvejhusentalukder.com>
2025-08-21 19:57:05 +0600

    Update: File Updated

---
+++ Rglr Practice/src/pracPackage/PassFail.java
@@ -0,0 +1,7 @@
+// package pracPackage;
+// import java.util.Scanner;
+// public class PassFail {
+//     public static void main(String[] args) {
+//         Scanner inp = new Scanner(System.in);
+//     }
+// }
---
+++ Rglr Practice/src/pracPackage/SaleryIncrement.java
@@ -0,0 +1,19 @@
+// package pracPackage;
+// import java.util.Scanner;
+// public class SaleryIncrement {
+//     public static void main(String[] args) {
+//         // Scanner toTake = new Scanner(System.in);
+//         // int EmployeeNumbers = toTake.nextInt();
+//         // // double[][] EmpSaleIncr = new double[EmployeeNumbers][2];
+//         double[][] emp = {
+//             {2000, 10},
+//             {3000, 13}
+//         };
+//         double Percentage, IncresedSalery, DiffSsalery;
+//         Percentage = 10 / 100.00;
+//         IncresedSalery = 2000 * Percentage;
+//         DiffSsalery = IncresedSalery + 2000;
+//         System.out.println(DiffSsalery);
+//         // toTake.close();
+//     }
+// }
