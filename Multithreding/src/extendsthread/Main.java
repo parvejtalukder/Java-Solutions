e6cea2b90cf2ad9c379679b96338afe391f7a291
parvejtalukder <mail@parvejhusentalukder.com>
2026-02-17 11:25:27 +0600

    Update: New sass

--- Multithreding/src/extendsthread/Main.java
+++ Multithreding/src/extendsthread/Main.java
@@ -3,14 +3,7 @@ package extendsthread;
 public class Main {
     public static void main(String[] args) {
         Model myThread = new Model();
-        Prototype hiThread = new Prototype();
-        myThread.start();
-        hiThread.setPriority(Thread.MAX_PRIORITY);
-        try {
-            myThread.join();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        hiThread.start();
+        Model hiThread = new Model();
+        // hiThread.start(); 
     }
 }
--- Multithreding/src/extendsthread/Model.java
+++ Multithreding/src/extendsthread/Model.java
@@ -1,29 +1,37 @@
 package extendsthread;
 
 public class Model extends Thread {
-    // int net_tickets;
+    int net_tickets = 1;
 
     @Override
     public void run() {
         // System.out.println("It's " + Thread.currentThread().getName());
         // Thread.currentThread().setName();
-        for(int i = 1; i <= 300; i++) {
-            System.out.println(i);
-            if (i % 10 == 0) {
-                try {
-                    System.out.println("Going to sleep for a bit..");
-                    sleep(1000);  
-                    try {
-                        System.out.println("........I am back!" + " " + Thread.currentThread().getName());
-                        sleep(300);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            // System.out.print("I am back!");
+            //         for(int i = 1; i <= 300; i++) {
+            // System.out.println(i);
+            // if (i % 10 == 0) {
+            //     try {
+            //         System.out.println("Going to sleep for a bit..");
+            //         sleep(1000);  
+            //         try {
+            //             System.out.println("........I am back!" + " " + Thread.currentThread().getName());
+            //             sleep(300);
+            //         } catch (Exception e) {
+            //             e.printStackTrace();
+            //         }
+            //     } catch (Exception e) {
+            //         e.printStackTrace();
+            //     }
+            // }
+            //}
+            // // System.out.print("I am back!");
+            // CallMe(String str);
+    }
+    public synchronized void CallMe(String nm) {
+        if (net_tickets > 0) {
+            System.out.println(nm + " Done!");
+            net_tickets--;
         }
+        else System.out.println("Sorry, No Ticket! " + nm);
     }
 }
