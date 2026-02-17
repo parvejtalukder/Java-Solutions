16a4d8c637b77ae88d4e6f5367d9c035a54512f7
parvejtalukder <mail@parvejhusentalukder.com>
2026-02-17 10:23:36 +0600

    UPdat: New Changs

---
+++ Multithreding/src/practiceone/Main.java
@@ -0,0 +1,8 @@
+package practiceone;
+
+public class Main {
+    public static void main(String[] args) {
+        Model myThread = new Model();
+        myThread.start();
+    }
+}
---
+++ Multithreding/src/practiceone/Model.java
@@ -0,0 +1,8 @@
+package practiceone;
+
+public class Model extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Hi, I' am Run");
+    }
+}
